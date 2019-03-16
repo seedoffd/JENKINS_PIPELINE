24623d7cd64d9a4a8637c5e5782edc132a633ebd
seedoffd <seedoffd@gmail.com>
2019-03-16 11:08:01 -0500

    this website

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,27 @@
 node {
-    stage("pull git")
+    stage("pull git"){
        git "git@github.com:seedoffd/Jenkins-website.git"
-}
+
+       }
+
+    stage("Install apache"){
+        sh "ssh ec2-user@${DEVIP}    sudo yum install httpd -y"
+
+        }
+
+    stage("start Apace"){
+        sh "ssh ec2-user@${DEVIP}    sudo systemctl start htppd"
+
+        }
+
+    stage("copy files"){
+        sh "rsync -aP --delete index.html    ec2-user@${DEVIP}:/tmp"
+
+        }
+
+    stage("Move files"){
+        sh "ssh ec2-user@${DEVIP}      sudo cp -f /tmp/index.html /var/www/html/index.html"
+
+        }
+
+    }
